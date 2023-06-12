6642584bf33a3165d1b49b46722ddee5cb9f0e48
leo <leowu102000@gmail.com>
2023-06-12 10:43:32 +0800

    add command

--- simple_socket/socket_client.py
+++ simple_socket/socket_client.py
@@ -3,6 +3,11 @@ import socket
 HOST = '127.0.0.1'
 PORT = 7000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#socket.AF_UNIX	於本機端進行串接
+#socket.AF_INET	於伺服器與伺服器之間進行串接
+#socket.AF_INET6	使用IPv6於伺服器與伺服器之間進行串接
+#socket.SOCK_STREAM	使用TCP提供可靠、雙向、串流的通信頻道
+#socket.SOCK_DGRAM	使用UDP通用的免連線訊息交換通道
 s.connect((HOST, PORT))
 
 while True:
--- simple_socket/socket_server.py
+++ simple_socket/socket_server.py
@@ -6,6 +6,14 @@ MAX_CONNECT_NUMBER = 5
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#socket.SOL_SOCKET Socket選項的基本層級，用於指定要設置或獲取的Socket選項的範圍或類型
+#SO_REUSEADDR選項來設置地址重用
+#1表示地址重用打開
+#Socket地址重用在以下情況下特別有用：
+
+#程序終止後立即重啟服務器，而不需要等待時間段的結束。
+#網絡故障或意外斷開後，重新建立連接時能夠快速重用之前的本地地址。
+
 s.bind((HOST, PORT))
 s.listen(MAX_CONNECT_NUMBER)
 
